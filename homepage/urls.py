c0e3864286d684d9fd46465d28b6aa08a721682c
behzodbek808 <behzodbekdadaboyev808@gmail.com>
2021-08-11 11:51:07 +0500

    update

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,4 @@ from .views import HomePageView, AboutPageView
 
 urlpatterns = [
  path('', HomePageView.as_view(), name='home'),
- path('about/', AboutPageView.as_view() , name='about'),
-]
+ path('about/', AboutPageView.as_view() , name='about'), ]
